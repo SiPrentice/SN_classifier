8c840e887eb280699d684d1415206aeee0424aa1
SiPrentice <prenno@gmail.com>
2021-04-08 09:52:32 +0100

    Housekeeping

--- fit_KNN_model.py
+++ fit_KNN_model.py
@@ -3,7 +3,7 @@
 """
 Created on Sun Jul 12 09:35:15 2020
 
-@author: Si
+@author: SPrentice
 """
 
 from sklearn.decomposition import PCA
@@ -22,7 +22,7 @@ from functions import spectrograph
 
 # set the number k of nearest neighours to find
 k = 1000
-
+#spectrograph  = '2021-03-04'
 
 #############
 
@@ -39,31 +39,8 @@ pca_folder = './PCA_ready/'
 X = np.loadtxt(pca_folder + 'PCAReady.%s.txt' %spectrograph )
 y = np.loadtxt(pca_folder + 'classlist.%s.txt' %spectrograph, dtype='str')
 
-
 X_train = X
 y_train = y
-
-# print('Performing pca')
-# pca=PCA(n_components=0.95)
-
-# pca.fit(X_train)
-
-
-# # now find the principal components
-# X_reduced =pca.fit_transform(X_train)
-
-# # This reconstructs the original data from the pca transform
-# X_return = pca.inverse_transform(X_reduced)
-
-# score = np.mean(cross_val_score(pca, X_reduced))
-# print(score)
-
-# plt.plot(np.arange(1, pca.n_components_ + 1),
-#           pca.explained_variance_ratio_, '+', linewidth=2)
-# plt.yscale('log')
-# plt.show()
-
-
 
 
 print('Fitting model')
@@ -71,21 +48,6 @@ clf = make_pipeline( StandardScaler(), PCA(n_components = 0.95),
                     KNeighborsClassifier(n_neighbors = k, weights = 'distance') )
 print(clf)
 clf.fit(X_train,y_train)
-
-
-
-# clf_nn = make_pipeline( StandardScaler(), PCA(n_components=0.95),
-#                     NearestNeighbors(n_neighbors=k) )
-# print(clf_nn)
-# clf_nn.fit(X_train,y_train)
-
-# pca_pipe = make_pipeline( StandardScaler(), PCA(n_components=0.95) )
-# pca_pipe.fit(X_train)
-
-# X_pca = pca_pipe.fit_transform(X_train)
-
-# model = KNeighborsClassifier(n_neighbors=k, weights = 'distance')
-# model.fit(X_pca, y_train)
 
 
 
@@ -97,8 +59,5 @@ if os.path.isdir('./for_classification') == False:
     print(f'Creating ./for_classification')
     os.mkdir('./for_classification')    
 
-#dump(clf_nn, 'NN_Classification_model.k=%s.joblib' %(k)) 
-# dump(model, 'KNNmodel.k=%s.joblib' %(k)) 
-# dump(pca_pipe, 'PCAfit.k=%s.joblib' %(k)) 
 
 
